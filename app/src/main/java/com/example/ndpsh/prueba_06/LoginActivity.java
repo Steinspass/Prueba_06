7b9c1e9d89e594d39f942b8c80cc1a7ec60fe376
Steinspass <37332713+Steinspass@users.noreply.github.com>
2018-11-14 19:52:19 +0100

    login

--- app/src/main/java/com/example/ndpsh/prueba_06/LoginActivity.java
+++ app/src/main/java/com/example/ndpsh/prueba_06/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.example.ndpsh.prueba_06;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
 
     private EditText textEmail;
     private EditText textPassword;
@@ -20,7 +27,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+
         bindUI();
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = textEmail.getText().toString();
+                String password = textPassword.getText().toString();
+
+               if (login(email, password)) {
+                   goToMain();
+                   saveOnPreferences(email, password);
+               }
+            }
+        });
 
     }
 
@@ -34,10 +55,38 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private boolean login(String email, String password) {
+        if (!isValidEmail(email)){
+            Toast.makeText(this, "Email is not valid, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else if (!isValidPassword(password)) {
+            Toast.makeText(this, "Password is not valid, 8 characters or  more please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private void saveOnPreferences(String email, String password) {
+        if (switchRemember.isChecked()) {
+           SharedPreferences.Editor editor = prefs.edit();
+           editor.putString("email", email);
+           editor.putString("pass", password);
+           editor.commit();
+           editor.apply();
+        }
+    }
+
     private boolean isValidEmail(String email) {
         return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
     private boolean isValidPassword(String password) {
-        return password.length() > 8;
+        return password.length() >= 8;
+    }
+
+    private void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
